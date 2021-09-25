3f7519152306e0cca822806c7b9ad00c67db457c
shahidmonowarr <shahidmonowar21@gmail.com>
2021-09-26 02:55:31 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="text-center">
+            <h2>Favourite Books Writer</h2>
+        </div>
+    );
+};
+
+export default Header;
